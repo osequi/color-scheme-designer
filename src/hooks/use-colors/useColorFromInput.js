cf302619c6891422919802cd5eecb3f18ffbc3b3
osequi <cs@osequi.com>
2020-10-25 06:06:29 +0200

    Working on the color hooks

--- src/hooks/use-colors/useColorFromInput.js
+++ src/hooks/use-colors/useColorFromInput.js
@@ -1,9 +1,26 @@
 import chroma from "chroma-js";
 
+/**
+ * Creates color form an input and a space
+ * @param  {string} input The color definition.
+ * @param  {string} space The color space.
+ * @return {object}       A chroma.js compatible color, or an error message.
+ * @example input: 255, 255, 255 space: rgb => chroma('white')
+ */
 const createColorFromInput = (input, space) => {
   if (!input || !space) return null;
 
-  return "Invalid input. Please check https://gka.github.io/chroma.js/#chroma for the syntax.";
+  const err =
+    "Invalid input. Please check https://gka.github.io/chroma.js/#chroma for the syntax.";
+
+  const removedWhiteSpace = input.split(" ").join("");
+  const parts = removedWhiteSpace.split(",");
+  if (parts.length !== 3) return err;
+
+  const c = chroma(parts[0], parts[1], parts[2], space);
+  console.log("c:", c);
+
+  return c;
 };
 
 const useColorFromInput = (input, space) => {
